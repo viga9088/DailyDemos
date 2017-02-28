4bb388097c1858afbcb546af0514c2a684d8a0ee
viga_git <vemon9088@gmail.com>
2017-02-28 00:41:24 -0500

    daily update 170228

--- Php/demo170227/main.php
+++ Php/demo170227/main.php
@@ -5,3 +5,23 @@
  * Date: 2017/2/27
  * Time: PM10:09
  */
+
+/**
+ * __toString() 把一个class对象以string形式输出
+ */
+class Magic{
+    public function __toString()
+    {
+        // TODO: Implement __toString() method.
+        return "This is the toString()";
+    }
+
+    public function __invoke($x)
+    {
+        // TODO: Implement __invoke() method.
+        echo "__invoke function called with the parameter " . $x . ".\n";
+    }
+}
+
+$obj = new Magic();
+echo $obj;
